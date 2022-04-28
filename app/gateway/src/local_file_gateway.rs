64a10cea9745dd00fbf1cfb5f9e1bd6cb066f890
ragnar1904 <apollo.oth@gmail.com>
2022-04-28 23:23:18 +0900

    :bug: forgot &self

--- app/gateway/src/local_file_gateway.rs
+++ app/gateway/src/local_file_gateway.rs
@@ -3,7 +3,7 @@ use port::local_file_port::LocalFilePort;
 pub struct LocalFileGateway;
 
 impl LocalFilePort for LocalFileGateway {
-    fn create_file() -> anyhow::Result<()> {
+    fn create_file(&self) -> anyhow::Result<()> {
         todo!()
     }
 }
