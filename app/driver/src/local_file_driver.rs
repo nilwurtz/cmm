c8165091c7d12f58a68147253b390966b1086984
ragnar1904 <apollo.oth@gmail.com>
2022-06-21 00:58:46 +0900

    :+1: create data dir

--- app/driver/src/local_file_driver.rs
+++ app/driver/src/local_file_driver.rs
@@ -3,9 +3,25 @@ use mockall::automock;
 #[automock]
 #[allow(clippy::module_inception)]
 pub mod local_file_driver {
+    use dirs::home_dir;
+    use std::fs;
+
     pub fn create_dir() -> anyhow::Result<(), domain::errors::FileError> {
-        todo!()
+        let dir_path = home_dir().unwrap().as_path().join(".cmm");
+        match fs::create_dir_all(dir_path.clone()) {
+            Ok(_) => {
+                println!("Create data dir {}", dir_path.to_str().unwrap().to_string());
+                Ok(())
+            }
+            Err(e) => {
+                println!("Skip to create {}", dir_path.to_str().unwrap().to_string());
+                Err(domain::errors::FileError::CreateFailedError(
+                    format!("Failed to create {:?}", e.kind(),).to_string(),
+                ))
+            }
+        }
     }
+
     pub fn create_file(_file_name: String) -> anyhow::Result<(), domain::errors::FileError> {
         todo!()
     }
